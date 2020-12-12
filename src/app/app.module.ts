8b8bf3c0aaa81697543ea51aa35dd2349b118cad
memonkme <memonkme19@rediffmail.com>
2020-12-12 17:52:10 +0530

    5 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { UserCardComponent } from "./user-card/user-card.component";
 
 @NgModule({
-  imports: [BrowserModule, ReactiveFormsModule],
-  declarations: [AppComponent,HelloComponent, UserCardComponent],
+  imports: [BrowserModule, ReactiveFormsModule, HttpClientModule],
+  declarations: [AppComponent, HelloComponent, UserCardComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-user-card',
-  templateUrl: './user-card.component.html',
-  styleUrls: ['./user-card.component.css']
+  selector: "app-user-card",
+  templateUrl: "./user-card.component.html",
+  styleUrls: ["./user-card.component.css"]
 })
 export class UserCardComponent implements OnInit {
-
-  constructor() { }
+  userList: any;
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.http
+      .get(
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json"
+      )
+      .subscribe(data => {
+        this.userList = data;
+        console.log(data);
+      });
   }
-
 }
