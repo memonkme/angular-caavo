d82c278dec4a1261a2b01577955b33abb57f5a72
memonkme <memonkme19@rediffmail.com>
2020-12-12 17:19:52 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  ViewChild
+} from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  name = "Angular";
+  fileToUpload: any;
+  imageUrl: any = '';
+  isUploaded: boolean = false;
+  removeUpload: boolean;
+  constructor(private cd: ChangeDetectorRef) {}
+  @ViewChild("Image") el: ElementRef;
+
+  handleFileInput(event) {
+    let reader = new FileReader(); // HTML5 FileReader API
+    let file = event.target.files[0];
+    if (event.target.files && event.target.files[0]) {
+      reader.readAsDataURL(file);
+
+      // When file uploads set it to file formcontrol
+      reader.onload = () => {
+        this.imageUrl = reader.result;
+        this.isUploaded = true;
+      };
+      // ChangeDetectorRef since file is loading outside the zone
+      this.cd.markForCheck();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HelloComponent } from "./hello.component";
 import { UserCardComponent } from "./user-card/user-card.component";
 
 @NgModule({
-  imports: [BrowserModule, FormsModule],
-  declarations: [HelloComponent, UserCardComponent],
+  imports: [BrowserModule, ReactiveFormsModule],
+  declarations: [AppComponent,HelloComponent, UserCardComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
