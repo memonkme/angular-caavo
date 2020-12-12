357d5cab03f1c60064ebd9d26c380a65bb55a316
memonkme <memonkme19@rediffmail.com>
2020-12-12 22:13:02 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
+import { HttpClient } from "@angular/common/http";
 import {
   ChangeDetectorRef,
   Component,
   ElementRef,
+  OnInit,
   ViewChild
 } from "@angular/core";
 
@@ -10,14 +12,37 @@ import {
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   name = "Angular";
   fileToUpload: any;
-  imageUrl: any = '';
+  imageUrl: any = "";
   isUploaded: boolean = false;
   removeUpload: boolean;
-  constructor(private cd: ChangeDetectorRef) {}
+  userList: any;
+  constructor(private cd: ChangeDetectorRef, private http: HttpClient) {}
   @ViewChild("Image") el: ElementRef;
+
+  ngOnInit() {
+    this.http
+      .get(
+        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json"
+      )
+      .subscribe(data => {
+        this.userList = data;
+        console.log(data);
+      });
+  }
+  sortUserList() {
+    this.userList.sort(function(a, b) {
+      if (a.name < b.name) {
+        return -1;
+      }
+      if (a.name > b.name) {
+        return 1;
+      }
+      return 0;
+    });
+  }
 
   handleFileInput(event) {
     let reader = new FileReader(); // HTML5 FileReader API
--- src/app/user-card/user-card.component.ts
+++ src/app/user-card/user-card.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from "@angular/common/http";
-import { Component, OnInit } from "@angular/core";
+import { Component, Input,  OnInit } from "@angular/core";
 
 @Component({
   selector: "app-user-card",
@@ -7,31 +7,20 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./user-card.component.css"]
 })
 export class UserCardComponent implements OnInit {
-  userList: any;
+  @Input() userList: any;
   selectedUser: any = [];
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
-    this.http
-      .get(
-        "https://s3-ap-southeast-1.amazonaws.com/he-public-data/users49b8675.json"
-      )
-      .subscribe(data => {
-        this.userList = data;
-        console.log(data);
-      });
   }
   selectUser(user) {
     console.log("User Card Clicked", user);
     if (this.selectedUser.includes(user)) {
-      let temp = this.selectedUser.filter(id => {
-        return id != user;
-      });
+      this.selectedUser.splice(this.selectedUser.indexOf(user), 1);
       document.getElementById(user).setAttribute("class", "d-none");
-      this.selectUser = temp;
     } else {
-      document.getElementById(user).setAttribute("class", "selectedUser");
       this.selectedUser.push(user);
+      document.getElementById(user).setAttribute("class", "selectedUser");
     }
   }
 }
